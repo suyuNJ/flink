fcd888bffa869f9ceea680c4078530f5beea2cb3
suyu <suy55@chinaunicom.com>
2021-08-27 16:05:42 +0800

    添加批量处理 方法二

---
+++ iot/rule-engine/src/main/java/cn/suyu/iot/ruleengine/function/SetMapFunction.java
@@ -0,0 +1,20 @@
+package cn.suyu.iot.ruleengine.function;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * @Description TODO
+ * @Author suyu
+ * @Data 2021/8/27 15:38
+ */
+public class SetMapFunction implements FlatMapFunction<String, Tuple2<String, Integer>> {
+    @Override
+    public void flatMap(String value, Collector<Tuple2<String, Integer>> out) throws Exception {
+        String[] words = value.split(" ");
+        for (String word : words) {
+            out.collect(new Tuple2<>(word, 1));
+        }
+    }
+}
--- iot/rule-engine/src/main/java/cn/suyu/iot/ruleengine/job/RuleEngineDataSetJob.java
+++ iot/rule-engine/src/main/java/cn/suyu/iot/ruleengine/job/RuleEngineDataSetJob.java
@@ -1,12 +1,10 @@
 package cn.suyu.iot.ruleengine.job;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.MapFunction;
+import cn.suyu.iot.ruleengine.function.SetMapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 
 /**
  * @Description 读取指定路径文件作为输入源，做批处理
@@ -22,25 +20,26 @@ public class RuleEngineDataSetJob {
         String inputPath = "D:\\dev\\src\\mygithub\\flink\\iot\\rule-engine\\src\\main\\resources\\hello.txt";
         DataSource<String> inputDataSource = env.readTextFile(inputPath);
 
-        //对数据进行转换处理统计，先分词，再按照word进行分组，最后聚合统计
-        DataSet<Tuple2<String, Integer>> dataSet = inputDataSource.flatMap(new FlatMapFunction<String, String>() {
-            public void flatMap(String s, final Collector<String> collector) throws Exception {
-                final String[] words = s.split(" ");
-                for (String str : words) {
-                    collector.collect(str);
-                }
-            }
-        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+        // 对数据进行转换处理统计，先分词，再按照word进行分组，最后聚合统计
+        //方法 1.
+//        DataSet<Tuple2<String, Integer>> dataSet = inputDataSource.flatMap(new FlatMapFunction<String, String>() {
+//            public void flatMap(String s, final Collector<String> collector) throws Exception {
+//                final String[] words = s.split(" ");
+//                for (String str : words) {
+//                    collector.collect(str);
+//                }
+//            }
+//        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+//
+//            public Tuple2<String, Integer> map(String s) throws Exception {
+//                Tuple2<String, Integer> countTuple = new Tuple2<String, Integer>();
+//                countTuple.setFields(s, 1);
+//                return countTuple;
+//            }
+//        }).groupBy(0).sum(1);
 
-            public Tuple2<String, Integer> map(String s) throws Exception {
-                Tuple2<String, Integer> countTuple = new Tuple2<String, Integer>();
-                countTuple.setFields(s, 1);
-                return countTuple;
-            }
-        }).groupBy(0).sum(1);
-
+        //方法2
+        DataSet<Tuple2<String, Integer>> dataSet = inputDataSource.flatMap(new SetMapFunction()).groupBy(0).sum(1);
         dataSet.print();
-
-
     }
 }
